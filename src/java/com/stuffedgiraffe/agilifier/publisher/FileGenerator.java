004f584d5d0b446b54a061a930c407342d77b38b
joneswm <joneswm@366324b9-161f-0410-a701-fc36a7dbc6d5>
2007-02-20 14:48:07 +0000

    Finished reorganisation to put the stuff under trunk!

--- src/java/com/stuffedgiraffe/agilifier/publisher/FileGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.stuffedgiraffe.agilifier.publisher;
-
-import java.io.File;
-import java.util.Map;
-
-public interface FileGenerator {
-    void generateFile(String templateName, Map context, File outputFile);
-}
